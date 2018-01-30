3870eb61b2bf5065d4d18f05b8b941f284562da9
BigbyHu <1105608674@qq.com>
2018-01-30 17:56:44 +0800

    Update TestGrid.java

--- src/test/java/org/testJenkins/TestGrid.java
+++ src/test/java/org/testJenkins/TestGrid.java
@@ -35,8 +35,8 @@ public class TestGrid {
 		//String Node = "http://192.168.40.73:4444/wd/hub";
 		//String Node = "http://172.17.0.7:4444/wd/hub";
 		String Node = "http://selenium-hub:4444/wd/hub";
-		//String URL = "http://tores3-test.router.default.svc.cluster.local/TORES/";
-		String URL = "https://www.baidu.com";
+		String URL = "http://tores3-test.router.default.svc.cluster.local/TORES/";
+		//String URL = "https://www.baidu.com";
 		if (browser.equalsIgnoreCase("firefox")) {
 			System.out.println(" Executing on FireFox");
 			DesiredCapabilities cap = DesiredCapabilities.firefox();
