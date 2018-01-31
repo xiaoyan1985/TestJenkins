99ced66686d642bebed02b8e1e425a3fcaa63f6c
BigbyHu <1105608674@qq.com>
2018-01-31 14:42:18 +0800

    Update TestGrid.java

--- src/test/java/org/testJenkins/TestGrid.java
+++ src/test/java/org/testJenkins/TestGrid.java
@@ -45,7 +45,7 @@ public class TestGrid {
 			cap.setBrowserName("firefox");
 			driver = new RemoteWebDriver(new URL(Node), cap);
 			driver.navigate().to(URL);
-			//driver.manage().window().setSize(new Dimension(1360, 1020)); 
+			driver.manage().window().setSize(new Dimension(136, 102)); 
 		} else if (browser.equalsIgnoreCase("chrome")) {
 			System.out.println(" Executing on CHROME");
 			DesiredCapabilities cap = DesiredCapabilities.chrome();
