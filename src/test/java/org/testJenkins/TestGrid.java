301ea8418d99a15b2f22eb2e5b42b0cf2e7af0e0
xiaoyan1985 <32161110+xiaoyan1985@users.noreply.github.com>
2018-03-13 16:22:44 +0800

    Update TestGrid.java

--- src/test/java/org/testJenkins/TestGrid.java
+++ src/test/java/org/testJenkins/TestGrid.java
@@ -34,11 +34,11 @@ public class TestGrid {
 		//String URL = "http://springmvc-wfproject5.openshift.axies.org/springmvc/";
 		//String Node = "http://192.168.40.73:4444/wd/hub";
 		String URL = "http://www.baidu.com";
-		//String Node = "http://selenium-hub:4444/wd/hub";
+		String Node = "http://selenium-hub:4444/wd/hub";
 		//String URL = "http://tores3-test.router.default.svc.cluster.local/TORES/index.jsp";
 		//String URL = "http://www.yahoo.co.jp";
 		//String URL = "http://172.17.0.10:8080/TORES/";
-		String Node = "http://10.131.1.112:4444/wd/hub";
+		//String Node = "http://10.131.1.112:4444/wd/hub";
 		
 		if (browser.equalsIgnoreCase("firefox")) {
 			System.out.println(" Executing on FireFox");
