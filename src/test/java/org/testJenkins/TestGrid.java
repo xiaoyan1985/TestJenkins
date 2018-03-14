2840bfafc11fa7e7638f788bf2fff1e223424e27
xiaoyan1985 <32161110+xiaoyan1985@users.noreply.github.com>
2018-03-14 09:38:14 +0800

    Update TestGrid.java

--- src/test/java/org/testJenkins/TestGrid.java
+++ src/test/java/org/testJenkins/TestGrid.java
@@ -34,7 +34,8 @@ public class TestGrid {
 		//String URL = "http://springmvc-wfproject5.openshift.axies.org/springmvc/";
 		//String Node = "http://192.168.40.73:4444/wd/hub";
 		String URL = "http://www.baidu.com";
-		String Node = "http://selenium-hub:4444/wd/hub";
+		//String Node = "http://selenium-hub:4444/wd/hub";
+		String Node = "http://10.130.1.126:4444/wd/hub";
 		//String URL = "http://tores3-test.router.default.svc.cluster.local/TORES/index.jsp";
 		//String URL = "http://www.yahoo.co.jp";
 		//String URL = "http://172.17.0.10:8080/TORES/";
