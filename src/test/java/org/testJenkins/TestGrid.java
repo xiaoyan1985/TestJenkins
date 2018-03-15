f0104f2c84d46e08fe5ebc74aa300e278ac42d70
xiaoyan1985 <32161110+xiaoyan1985@users.noreply.github.com>
2018-03-15 11:54:55 +0800

    Update TestGrid.java

--- src/test/java/org/testJenkins/TestGrid.java
+++ src/test/java/org/testJenkins/TestGrid.java
@@ -38,7 +38,7 @@ public class TestGrid {
 		String Node = "http://10.130.1.150:4444/wd/hub";
 		//String URL = "http://tores3-test.router.default.svc.cluster.local/TORES/index.jsp";
 		//String URL = "http://www.yahoo.co.jp";
-		String URL = "http://10.130.1.160:8080/TORES/";
+		String URL = "http://10.130.1.166:8080/TORES/";
 		//String Node = "http://10.131.1.112:4444/wd/hub";
 		
 		if (browser.equalsIgnoreCase("firefox")) {
